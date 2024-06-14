e8635623f66911f8022f2b4766e68e70c27285b9
chen1238 <65192478+chen1238@users.noreply.github.com>
2024-06-14 08:37:45 +0800

    Update 1.sh

--- Re-sequencing, read mapping and SNP calling/Identical scores/1.sh
+++ Re-sequencing, read mapping and SNP calling/Identical scores/1.sh
@@ -18,6 +18,7 @@ vcftools --vcf ../x-1.recode.vcf --recode --mac 3 --max-missing 0.9 --out x
 #operate
 perl get_identical_score.pl  -i  ./chr/chrX.xls -c chr-x.tab -g group1.list -w 1000000 -s 1000000 -o ./identical_score/X.xls
 
+#
 #rename
 for i in {1..19} X;do mv ./identical_score/$i.xls  ./identical_score/$i.txt ;done
 
