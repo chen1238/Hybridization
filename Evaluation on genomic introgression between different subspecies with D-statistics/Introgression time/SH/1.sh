d5ef41c85f95eb827e0365c58516738091031bcc
chen1238 <65192478+chen1238@users.noreply.github.com>
2024-07-16 01:14:03 +0800

    Update 1.sh

--- Evaluation on genomic introgression between different subspecies with D-statistics/Introgression time/SH/1.sh
+++ Evaluation on genomic introgression between different subspecies with D-statistics/Introgression time/SH/1.sh
@@ -8,4 +8,4 @@ do
    cd ..
 done
  
-cat run{1..20}/autosome/autosome.bestlhoods | grep -v MaxObsLhood | awk '{print NR,$8}' | sort -k 2 >rank.txt
+cat run{1..100}/autosome-1/autosome-1.bestlhoods > SH.txt
